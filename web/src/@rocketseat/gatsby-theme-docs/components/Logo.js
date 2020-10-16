db3dbc198ce9c7c0d150e3e279b15799d98ab3ac
emmsil <emil.gunnarsson@mms.is>
2020-10-16 15:00:25 +0000

    web

---
+++ web/src/@rocketseat/gatsby-theme-docs/components/Logo.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import MMS from "../../../../static/mms-logo.png"
+
+export default function Logo(props) {
+  return (
+    <img src={MMS} width={180} height={34}></img>
+  );
+}
